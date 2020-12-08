6707e505b57976787b02efb78de81aaab85d4eaf
zhangpeng.eic <zhangpeng.eic@bytedance.com>
2020-12-08 18:36:47 +0800

    MOD: 修改一些配置
    Change-Id: Ie871a66b8476b81fd7f6c9ccfa686a8a60a6b010

--- start.sh
+++ start.sh
@@ -1,31 +1,46 @@
 #!/bin/bash
 
+echo "docker-compose -f docker-compose.yml up -d namenode datanode1 datanode2 datanode3 resourcemanager nodemanager1 historyserver " 
 docker-compose -f docker-compose.yml up -d namenode datanode1 datanode2 datanode3 resourcemanager nodemanager1 historyserver
 
+echo "sleep 5"
 sleep 5
 
+echo "docker-compose -f docker-compose.yml up -d hive-metastore-postgresql hive-server hive-metastore hive-metastore-postgresql mysql-server"
 docker-compose -f docker-compose.yml up -d hive-metastore-postgresql hive-server hive-metastore hive-metastore-postgresql mysql-server
 
+echo "sleep 5"
 sleep 5
 
+echo "docker-compose -f docker-compose.yml up -d spark-master spark-worker"
 docker-compose -f docker-compose.yml up -d spark-master spark-worker
 
+echo "sleep 5"
 sleep 5
 
+echo "docker-compose -f docker-compose.yml up -d spark-master spark-worker"
 docker-compose -f docker-compose.yml up -d spark-master spark-worker
 
+echo "sleep 5"
 sleep 5
 
+echo "docker-compose -f docker-compose.yml up -d elasticsearch kibana"
 docker-compose -f docker-compose.yml up -d elasticsearch kibana
 
+echo "sleep 5"
 sleep 5
 
+echo "docker-compose -f docker-compose.yml up -d zookeeper kafka kafka-manager"
 docker-compose -f docker-compose.yml up -d zookeeper kafka kafka-manager
 
+echo "sleep 5"
 sleep 5
 
+echo "docker-compose -f docker-compose.yml up -d zookeeper kafka kafka-manager"
 docker-compose -f docker-compose.yml up -d zookeeper kafka kafka-manager
 
+echo "sleep 5"
 sleep 5
 
+echo "docker-compose -f docker-compose.yml up -d jobmanager taskmanager"
 docker-compose -f docker-compose.yml up -d jobmanager taskmanager
