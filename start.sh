629bb73bb8885c18982e480c79857e85376cf61e
zhangpeng.eic <zhangpeng.eic@bytedance.com>
2020-12-08 18:51:06 +0800

    MOD: remove duplicate and flink conflict port etc
    Change-Id: I2e818ff973a6b037cfa1743de32af58821044995

--- start.sh
+++ start.sh
@@ -12,11 +12,11 @@ docker-compose -f docker-compose.yml up -d hive-metastore-postgresql hive-server
 echo "sleep 5"
 sleep 5
 
-echo "docker-compose -f docker-compose.yml up -d spark-master spark-worker"
-docker-compose -f docker-compose.yml up -d spark-master spark-worker
+# echo "docker-compose -f docker-compose.yml up -d spark-master spark-worker"
+# docker-compose -f docker-compose.yml up -d spark-master spark-worker
 
-echo "sleep 5"
-sleep 5
+# echo "sleep 5"
+# sleep 5
 
 echo "docker-compose -f docker-compose.yml up -d spark-master spark-worker"
 docker-compose -f docker-compose.yml up -d spark-master spark-worker
@@ -30,17 +30,17 @@ docker-compose -f docker-compose.yml up -d elasticsearch kibana
 echo "sleep 5"
 sleep 5
 
-echo "docker-compose -f docker-compose.yml up -d zookeeper kafka kafka-manager"
-docker-compose -f docker-compose.yml up -d zookeeper kafka kafka-manager
+# echo "docker-compose -f docker-compose.yml up -d zookeeper kafka kafka-manager"
+# docker-compose -f docker-compose.yml up -d zookeeper kafka kafka-manager
 
-echo "sleep 5"
-sleep 5
+# echo "sleep 5"
+# sleep 5
 
-echo "docker-compose -f docker-compose.yml up -d zookeeper kafka kafka-manager"
-docker-compose -f docker-compose.yml up -d zookeeper kafka kafka-manager
+# echo "docker-compose -f docker-compose.yml up -d zookeeper kafka kafka-manager"
+# docker-compose -f docker-compose.yml up -d zookeeper kafka kafka-manager
 
-echo "sleep 5"
-sleep 5
+# echo "sleep 5"
+# sleep 5
 
-echo "docker-compose -f docker-compose.yml up -d jobmanager taskmanager"
-docker-compose -f docker-compose.yml up -d jobmanager taskmanager
+# echo "docker-compose -f docker-compose.yml up -d jobmanager taskmanager"
+# docker-compose -f docker-compose.yml up -d jobmanager taskmanager
